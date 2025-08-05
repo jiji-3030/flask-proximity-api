b50903e38566ce23e198c096df7452561652430f
jiji-3030 <157555761+jiji-3030@users.noreply.github.com>
2025-08-05 22:28:44 +0800

    port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from geopy.distance import geodesic
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -21,5 +22,5 @@ def check_proximity():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5002))
     app.run(debug=False, host="0.0.0.0", port=port)
